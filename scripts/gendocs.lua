a8a304555c51bcf7f601c7bf86f2bb8b490cd685
Simon Hauser <Simon-Hauser@outlook.de>
2021-01-05 19:25:52 +0100

    Add gendocs ci workflow

--- scripts/gendocs.lua
+++ scripts/gendocs.lua
@@ -1,14 +1,11 @@
-RELOAD('docgen')
-
 local docgen = require('docgen')
 
 local docs = {}
 
 docs.test = function()
-  local input_dir = "./lua/telescope/"
-  local input_files = vim.fn.globpath(input_dir, "**/*.lua", false, true)
-
-  input_files = { "./lua/telescope/init.lua" }
+  local input_files = {
+    "./lua/telescope/init.lua"
+  }
 
   local output_file = "./docs/telescope.txt"
   local output_file_handle = io.open(output_file, "w")
@@ -17,7 +14,7 @@ docs.test = function()
     docgen.write(input_file, output_file_handle)
   end
 
-  output_file_handle:write(" vim:tw=78:ts=8:ft=help:norl:")
+  output_file_handle:write(" vim:tw=78:ts=8:ft=help:norl:\n")
   output_file_handle:close()
   vim.cmd [[checktime]]
 end
